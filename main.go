276b5c19eebaae854df0a31d6ed44ac5ebb267c5
Matthew Roser <mr.mattros@gmail.com>
2022-01-17 21:03:30 -0500

    began adding user input

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
-type BoardPosition struct {
-	gamePiece string
-	color     string
+var verticalInput = [8]int{
+	8, 7, 6, 5, 4, 3, 2, 1,
+}
+
+var horizontalInput = [8]string{
+	"a", "b", "c", "d", "e", "f", "g", "h",
 }
 
 type ChessPiece struct {
@@ -14,6 +21,7 @@ type ChessPiece struct {
 
 func main() {
 	//initialize values here
+	var selectedPiece string
 
 	//game loop
 	// for true {
@@ -21,7 +29,11 @@ func main() {
 	// }
 
 	//testing area
-	draw_gameboard(create_gameboard())
+	gameBoard := create_gameboard()
+	draw_gameboard(gameBoard)
+	fmt.Printf("\nEnter the position of a piece to move.")
+	fmt.Scanf("\nPosition: %s", &selectedPiece)
+
 }
 
 func draw_gameboard(gameboard [8][8]ChessPiece) {
@@ -36,6 +48,41 @@ func draw_gameboard(gameboard [8][8]ChessPiece) {
 		}
 		fmt.Printf("\n  ---------------------------------\n")
 	}
+
+}
+
+func get_piece(
+	selectedPiece string,
+	gameboard [8][8]ChessPiece) ChessPiece {
+
+	//break this into multiple functions later
+
+	var verticalPosition int
+	var horizontalPosition int
+
+	if len(selectedPiece) != 2 {
+		//return error, add later
+	}
+
+	for i := 0; i < len(selectedPiece); i++ {
+		if i == 0 {
+			j := selectedPiece[i]
+			k, err := strconv.Atoi(
+				string(j))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			verticalPosition = get_vertical_input_element_valu(k)
+		}
+		if i == 1 {
+			k := string(
+				selectedPiece[i])
+			horizontalPosition = get_horizontal_input_element_value(k)
+		}
+	}
+
+	return gameboard[verticalPosition][horizontalPosition]
 
 }
 
@@ -69,4 +116,32 @@ func create_gameboard() [8][8]ChessPiece {
 
 	return gameboard
 
+}
+
+//get better name
+func get_vertical_input_element_valu(elementValue int) int {
+	//clean this up, no need for a variable declaration before the loop
+	var value int
+	//add error handling incase its a bad user input
+	for i := 0; i < len(verticalInput); i++ {
+		if verticalInput[i] == elementValue {
+			value = i
+		}
+	}
+
+	return value
+}
+
+//get better name
+func get_horizontal_input_element_value(elementValue string) int {
+	//clean this up, no need for a variable declaration before the loop
+	var value int
+	//add error handling incase its a bad user input
+	for i := 0; i < len(horizontalInput); i++ {
+		if horizontalInput[i] == elementValue {
+			value = i
+		}
+	}
+
+	return value
 }
